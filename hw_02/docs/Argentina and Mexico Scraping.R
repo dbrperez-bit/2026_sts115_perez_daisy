de1e31770a89613f207cbd01baa63403487cd5b7
Daisy Perez <dbrperez@ucdavis.edu>
2026-02-05 16:06:36 -0800

    Correcting repo link and adding HW2 folders

---
+++ hw_02/docs/Argentina and Mexico Scraping.R
@@ -0,0 +1,6 @@
+install.packages("rvest")
+install.packages("dblyr")
+install.packages("stringr")
+targetyears =c("1961","1974","1985","1998","2004","2013")
+url = "https://www.minneapolisfed.org/research/staff-reports/inflation-money-and-output-under-alternative-monetary-standards"
+
